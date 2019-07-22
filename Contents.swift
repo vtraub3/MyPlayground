bc0c653e738b40c3b30ac2acc0d5b77ccd67280e
Apple <apple@Apples-MacBook-Pro-63.local>
2019-07-22 12:03:40 -0700

    initialize edgit

---
+++ Contents.swift
@@ -0,0 +1,81 @@
+var adress = " 1211 Mocking Bird Lane "
+adress = " 254 Ocean Avenue "
+var str = 5 // that is an interger
+var empty:Double // data types are always written with a capital. when your defining a variable use the equal sign before
+// print{"hello, world"}
+
+
+print ("Hello, world")
+
+
+
+
+
+import Cocoa
+
+var string = "Hello, playground"// created a string that prints hello playground
+// string= a series of characters that is inclosed by quatation marks Ex; "hello, world"
+// interger= numbers without a decimal
+// double= numbers that have decimals / numbers that store decimals. More accurate than float
+// interpolation
+// variable= the short definition for something
+// constant
+// keyword= words that were already in the codeing language, its most of the basic controls
+// a constant is a variable that never changes Ex; let [it will make the program faster/smoother]
+//
+
+var omg = "Oh my GLOB"
+var lol = "HAAaaaaAaaAaHAaaAaa "
+print (omg + " " + lol)
+print ( "Hello, chumps", (9+8))
+print ("hello chumps \(9+8)") //interpolation
+print ("slow typer \(7+8)" )
+
+
+
+//conditionals
+var temp = 87
+
+if temp < 80 {
+print ("wear pants")
+}
+
+
+
+var favoriteFood = "mac and cheese"
+
+if favoriteFood == "Chipotle" {
+print("My favorite food is Chipotle")
+} else if favoriteFood == "Starbucks" {
+    print("My favorite food is Starbucks")
+} else if favoriteFood == "mac and cheese"{
+    print("My favorite food is mac and cheese")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
